e127e26cde2d65b930a6e6fad308fe51feb0eabb
zcjone58 <3698106807@qq.com>
2026-06-27 08:25:10 +0800

    Update mario-less.c for input handling and output
    Fix input validation and adjust loop conditions for pyramid generation.

--- CS50x2023-Practice/Week1-C/ProblemSet1/Problem2/mario-less.c
+++ CS50x2023-Practice/Week1-C/ProblemSet1/Problem2/mario-less.c
@@ -3,5 +3,16 @@
 
 int main(void) 
 {
-    
+    int n;
+    do
+    {
+        n=get_int("请输入>=1且<=8的整数");
+    }while(n<=8$$n>=1);
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=0;k<n-i;k++) printf(" ");
+        for(int j=0;j<i;j++) printf("#");
+        printf("\n");
+    }
+    return 0;
 }
